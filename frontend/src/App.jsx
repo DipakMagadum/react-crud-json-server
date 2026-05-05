7b31783987a881ebfed09a6122f6474d1e28710a
dipak <deepakmagdum769@gmail.com>
2026-05-05 13:13:14 +0530

    refactor: improve navigation using Link components with proper styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
-import {Routes,Route} from 'react-router-dom'
+import {Routes,Route,Link} from 'react-router-dom'
+import DisplayData from './DisplayData'
+import AddNewData from './AddNewData'
 function App() {
 
   return (
    <>
-   <h1>React CRUD json server</h1>
+   <h1 className='text-center mt-2 '>React CRUD json server</h1>
+    <div className='flex justify-center gap-4'>
+      
+        <Link
+         className='bg-sky-500 p-1 rounded text-white' 
+        to="/">Display Data</Link>
+        
+        <Link
+         className='bg-sky-500 p-1 rounded text-white'
+         to="/add">Add New Data</Link>
+    </div>
+
+    <Routes>
+      <Route path='/' element={<DisplayData/>}/>
+       <Route path='/add' element={<AddNewData/>}/>
+    </Routes>
    </>
   )
 }
