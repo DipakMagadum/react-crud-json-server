02115db1fe3ba98d7970eb0c2fc6943ba6c7758a
dipak <deepakmagdum769@gmail.com>
2026-05-05 14:02:23 +0530

    feat: implement API data fetching and display student data in table

--- frontend/src/DisplayData.jsx
+++ frontend/src/DisplayData.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
 
+import { useEffect,useState } from 'react';
 function DisplayData() {
+
+  const [studentData, setStudentData] = useState([])
+
+  useEffect(()=>{
+    renderData();
+  },[])
+
+ async function renderData(){
+    const url = "http://localhost:3000/students";
+    let response =await fetch(url)
+    response = await response.json();
+    //console.log(response);
+
+    setStudentData(response)
+  }
   return (
-    <h1>Fetching Data From API</h1>
+   <>
+      <div className='flex justify-center items-center h-96'>
+          <table className='border border-black w-96 '>
+          <thead className='text-center border border-black'>
+            <tr>
+              <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>City</th>
+            </tr>
+          </thead>
+          <tbody className='text-center '>
+            {
+              studentData.map((students)=>(
+                <tr key={students.id} className='border border-black'>
+                  <td>{students.id}</td>
+                  <td>{students.name}</td>
+                  <td>{students.email}</td>
+                  <td>{students.city}</td>
+                  
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+   </>
+    
   )
 }
 
