ce243baeeb10b0ea7f0b82c060a9268e339f4805
dipak <deepakmagdum769@gmail.com>
2026-05-06 18:57:13 +0530

    feat: add controlled form state management for student data inputs

--- frontend/src/AddNewData.jsx
+++ frontend/src/AddNewData.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
-
+import {useState} from 'react'
 function AddNewData() {
+
+  const [id, setId] = useState('')
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [id, setCity] = useState('')
   return (
     <>
     <div className='flex justify-center items-center h-screen '>
@@ -8,31 +13,36 @@ function AddNewData() {
         className='flex flex-col gap-3 border border-gray-400  px-4 py-3 shadow-xl rounded  bg-white w-72'
       action="">
         <label >ID</label>
+
         <input 
-        className='border  border-gray-400 p-1 rounded-sm '
+          className='border  border-gray-400 p-1 rounded-sm '
           type="text"
           placeholder='enter your id '
+          onChange={(e)=>setId(e.target.value)}
         />
 
            <label >Name</label>
          <input 
-           className='border  border-gray-400 p-1 rounded-sm '
+          className='border  border-gray-400 p-1 rounded-sm '
           type="text"
           placeholder='enter your name '
+          onChange={(e)=>setName(e.target.value)}
         />
 
            <label >Email</label>
          <input 
-           className='border  border-gray-400 p-1 rounded-sm '
+          className='border  border-gray-400 p-1 rounded-sm '
           type="text"
           placeholder='enter your email '
+          onChange={(e)=>setEmail(e.target.value)}
         />
 
          <label >City</label>
          <input 
-           className='border  border-gray-400 p-1 rounded-sm '
+          className='border  border-gray-400 p-1 rounded-sm '
           type="text"
           placeholder='enter your city '
+          onChange={(e)=>setCity(e.target.value)}
         />
 
         <button
