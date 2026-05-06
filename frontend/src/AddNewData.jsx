824b5dfca7ce94b4c2c0e8de84650c3b50b5c434
dipak <deepakmagdum769@gmail.com>
2026-05-06 18:01:55 +0530

    feat: create form UI for adding new student data

--- frontend/src/AddNewData.jsx
+++ frontend/src/AddNewData.jsx
@@ -3,7 +3,43 @@ import React from 'react'
 function AddNewData() {
   return (
     <>
-    <h1>Add New Data into API</h1>
+    <div className='flex justify-center items-center h-screen '>
+      <form
+        className='flex flex-col gap-3 border border-gray-400  px-4 py-3 shadow-xl rounded  bg-white w-72'
+      action="">
+        <label >ID</label>
+        <input 
+        className='border  border-gray-400 p-1 rounded-sm '
+          type="text"
+          placeholder='enter your id '
+        />
+
+           <label >Name</label>
+         <input 
+           className='border  border-gray-400 p-1 rounded-sm '
+          type="text"
+          placeholder='enter your name '
+        />
+
+           <label >Email</label>
+         <input 
+           className='border  border-gray-400 p-1 rounded-sm '
+          type="text"
+          placeholder='enter your email '
+        />
+
+         <label >City</label>
+         <input 
+           className='border  border-gray-400 p-1 rounded-sm '
+          type="text"
+          placeholder='enter your city '
+        />
+
+        <button
+          className='bg-sky-500 p-1 rounded-lg text-white'
+        >Add New Data</button>
+      </form>
+    </div>
     </>
   )
 }
