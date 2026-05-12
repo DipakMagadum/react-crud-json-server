a619b3fcf4d5a3618256fc8c525dbe5f4b7180b8
dipak <deepakmagdum769@gmail.com>
2026-05-12 19:06:34 +0530

    refactor: update edit route to support dynamic student IDs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,27 @@
-import {Routes,Route,Link} from 'react-router-dom'
-import DisplayData from './DisplayData'
-import AddNewData from './AddNewData'
-import EditData from './EditData'
+import { Routes, Route, Link } from "react-router-dom";
+import DisplayData from "./DisplayData";
+import AddNewData from "./AddNewData";
+import EditData from "./EditData";
 function App() {
-
   return (
-   <>
-  
-    <div className='flex justify-center gap-4 mt-5'>
-      
-        <Link
-         className='bg-sky-500 p-1 rounded text-white' 
-        to="/">Display Data</Link>
-        
-        <Link
-         className='bg-sky-500 p-1 rounded text-white'
-         to="/add">Add New Data</Link>
-    </div>
+    <>
+      <div className="flex justify-center gap-4 mt-5">
+        <Link className="bg-sky-500 p-1 rounded text-white" to="/">
+          Display Data
+        </Link>
 
-    <Routes>
-      <Route path='/' element={<DisplayData/>}/>
-       <Route path='/add' element={<AddNewData/>}/>
-       <Route path='/edit' element={<EditData/>}/>
-    </Routes>
-   </>
-  )
+        <Link className="bg-sky-500 p-1 rounded text-white" to="/add">
+          Add New Data
+        </Link>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<DisplayData />} />
+        <Route path="/add" element={<AddNewData />} />
+        <Route path="/edit/:id" element={<EditData />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
