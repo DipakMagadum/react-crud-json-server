bba9f8fc398ebcd8dd3d50cb4b94e47b2e79849d
dipak <deepakmagdum769@gmail.com>
2026-05-12 19:41:36 +0530

    feat: fetch and display student data in edit form

--- frontend/src/EditData.jsx
+++ frontend/src/EditData.jsx
@@ -1,7 +1,33 @@
-
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 function EditData() {
+  const [studentId, setId] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [city, setCity] = useState("");
+
+  const params = useParams();
+
+  const navigate = useNavigate();
+const url = `http://localhost:3000/students/${params.id}`;
+
+  useEffect(() => {
+    getDataByIdInTextBox();
+  }, []);
+
+  const getDataByIdInTextBox = async () => {
+    let response = await fetch(url);
+    response = await response.json();
+    setId(response.studentId);
+    setName(response.name);
+    setEmail(response.email);
+    setCity(response.city);
+  };
+
   return (
-    <> <div className="flex justify-center items-center h-screen ">
+    <>
+      {" "}
+      <div className="flex justify-center items-center h-screen ">
         <form className="flex flex-col gap-3 border border-gray-400  px-4 py-3 shadow-xl rounded  bg-white w-72">
           <label className="text-sky-500 text-lg font-serif font-semibold">
             ID
@@ -10,6 +36,7 @@ function EditData() {
           <input
             className="border  border-gray-400 p-1 rounded-sm "
             type="text"
+            value={studentId}
             placeholder="enter your id "
             onChange={(e) => setId(e.target.value)}
           />
@@ -20,6 +47,7 @@ function EditData() {
           <input
             className="border  border-gray-400 p-1 rounded-sm "
             type="text"
+            value={name}
             placeholder="enter your name "
             onChange={(e) => setName(e.target.value)}
           />
@@ -30,6 +58,7 @@ function EditData() {
           <input
             className="border  border-gray-400 p-1 rounded-sm "
             type="email"
+            value={email}
             placeholder="enter your email "
             onChange={(e) => setEmail(e.target.value)}
           />
@@ -40,20 +69,18 @@ function EditData() {
           <input
             className="border  border-gray-400 p-1 rounded-sm "
             type="text"
+            value={city}
             placeholder="enter your city "
             onChange={(e) => setCity(e.target.value)}
           />
 
-          <button
-            className="bg-sky-500 p-1 rounded-lg text-white hover:bg-sky-700"
-          >
+          <button className="bg-sky-500 p-1 rounded-lg text-white hover:bg-sky-700">
             Update Data
           </button>
         </form>
       </div>
-    
     </>
-  )
+  );
 }
 
-export default EditData
+export default EditData;
